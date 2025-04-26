e23e0f5dd2f4b30f93595052143cc7d2ac13cf91
PassimElectus <kyle.a.lobato@gmail.com>
2025-04-25 18:00:21 -0700

    Commited on 4/25 at 6:00;

--- main.hpp
+++ main.hpp
@@ -7,7 +7,18 @@ void getTwoValues(int, int);
 void getNextPrime(int, int);
 void getPrevPrime(int, int);
 
-
+bool isPrime();
 // ******************************
 // Implement all your functions here
 // ******************************
+void getTwoValues(int n1, int n2)
+{
+    int n1, n2;
+    cin >> n1 >> n2; 
+    cout << n1 << " " << n2 << " " << endl;
+}
+
+void getPrevPime(int n1)
+{
+    
+}
