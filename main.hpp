17bea2b6b88549b9e940ee4ed535ad5191b75a86
PassimElectus <kyle.a.lobato@gmail.com>
2025-04-25 17:55:34 -0700

    Commited on 4/25 at 5:55pm;

--- main.hpp
+++ main.hpp
@@ -1,7 +1,12 @@
-
+#include <iostream>
+using namespace std;
 //********************
 // Declare Function Prototypes
 //********************
+void getTwoValues(int, int);
+void getNextPrime(int, int);
+void getPrevPrime(int, int);
+
 
 // ******************************
 // Implement all your functions here
